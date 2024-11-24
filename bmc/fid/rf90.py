07d9c36af1e4c86f8afe41d75c6df44b0a2ab1e3
dantona02 <danielmiksch3001@gmail.com>
2024-11-24 14:46:56 +0100

    Added rf90.py with FID Class and added simulate_fid()

--- bmc/fid/rf90.py
+++ bmc/fid/rf90.py
@@ -1,4 +1,84 @@
+from pathlib import Path
 import numpy as np
+import tqdm
+from typing import Tuple, Union
 from bmc.bmc_tool import BMCTool
 from bmc.bmc_tool import prep_rf_simulation
-import matplotlib.pyplot as plt
+from bmc.params import Params
+
+
+class FID(BMCTool):
+    def __init__(self, adc_time: np.float64, params: Params, seq_file: str | Path, verbose: bool = True, **kwargs) -> None:
+        super().__init__(params, seq_file, verbose, **kwargs)
+        """
+        Parameters
+        ----------
+        adc_time: np.float64
+            readout duration in seconds
+        """
+        self.adc_time = adc_time
+    
+    def run_adc(self, block, current_adc, accum_phase, mag) -> Tuple[int, float, np.ndarray]:
+
+        #adc with time dt and max_pulse_sampels
+        if block.adc is not None:
+            dt = self.adc_time / self.params.options["max_pulse_samples"]
+            for step in range(self.params.options["max_pulse_samples"]):
+                self.m_out[:, current_adc] = np.squeeze(mag)
+                accum_phase = 0
+                current_adc += 1
+
+                self.bm_solver.update_matrix(0, 0, 0)
+                mag = self.bm_solver.solve_equation(mag=mag, dtp=dt)
+            
+            # RF pulse
+        elif block.rf is not None:
+            amp_, ph_, dtp_, delay_after_pulse = prep_rf_simulation(block, self.params.options["max_pulse_samples"])
+            for i in range(amp_.size):
+                self.bm_solver.update_matrix(
+                    rf_amp=amp_[i],
+                    rf_phase=-ph_[i] + block.rf.phase_offset - accum_phase,
+                    rf_freq=block.rf.freq_offset,
+                )
+                mag = self.bm_solver.solve_equation(mag=mag, dtp=dtp_)
+
+            if delay_after_pulse > 0:
+                self.bm_solver.update_matrix(0, 0, 0)
+                mag = self.bm_solver.solve_equation(mag=mag, dtp=delay_after_pulse)
+
+            phase_degree = dtp_ * amp_.size * 360 * block.rf.freq_offset
+            phase_degree %= 360
+            accum_phase += phase_degree / 180 * np.pi
+
+        return current_adc, accum_phase, mag
+
+    def run_fid(self) -> None:
+        """
+        Start simulation process.
+        """
+        if self.n_offsets != self.n_measure:
+            self.run_m0_scan = True
+
+        current_adc = 0
+        accum_phase = 0
+        mag = self.m_init[np.newaxis, :, np.newaxis]
+
+        try:
+            block_events = self.seq.block_events
+        except AttributeError:
+            block_events = self.seq.dict_block_events
+
+        if self.verbose:
+            loop_block_events = tqdm(range(1, len(block_events) + 1), desc="BMCTool simulation")
+        else:
+            loop_block_events = range(1, len(block_events) + 1)
+
+        # code for pypulseq >= 1.4.0:
+        try:
+            for block_event in loop_block_events:
+                block = self.seq.get_block(block_event)
+                current_adc, accum_phase, mag = self.run_adc(block, current_adc, accum_phase, mag)
+        except AttributeError:
+            for block_event in loop_block_events:
+                block = self.seq.get_block(block_event)
+                current_adc, accum_phase, mag = self.run_1_3_0(block, current_adc, accum_phase, mag)
--- bmc/simulate.py
+++ bmc/simulate.py
@@ -5,8 +5,10 @@ simulate.py
 
 from pathlib import Path
 from typing import Union
+import numpy as np
 
 from bmc.bmc_tool import BMCTool
+from bmc.fid.rf90 import FID
 from bmc.set_params import load_params
 from bmc.utils.eval import plot_z
 
@@ -46,6 +48,56 @@ def simulate(config_file: Union[str, Path], seq_file: Union[str, Path], show_plo
     # create BMCTool object and run simulation
     sim = BMCTool(sim_params, seq_file, **kwargs)
     sim.run()
+    
+
+    if show_plot:
+        if "offsets" in kwargs:
+            offsets = kwargs.pop("offsets")
+            _, m_z = sim.get_zspec()
+        else:
+            offsets, m_z = sim.get_zspec()
+
+        plot_z(m_z=m_z, offsets=offsets, **kwargs)
+
+    return sim
+
+
+def simulate_fid(config_file: Union[str, Path], seq_file: Union[str, Path], adc_time: np.float64, show_plot: bool = False, **kwargs) -> FID:
+    """
+    simulate Run BMCTool simulation based on given seq-file and config file.
+
+    Parameters
+    ----------
+    config_file : Union[str, Path]
+        Path to the config file.
+    seq_file : Union[str, Path]
+        Path to the seq-file.
+    show_plot : bool, optional
+        Flag to activate plotting of simulated data, by default False
+
+    Returns
+    -------
+    BMCTool
+        BMCTool object containing the simulation results.
+
+    Raises
+    ------
+    FileNotFoundError
+        If the config_file or seq_file is not found.
+    """
+    if not Path(config_file).exists():
+        raise FileNotFoundError(f"File {config_file} not found.")
+
+    if not Path(seq_file).exists():
+        raise FileNotFoundError(f"File {seq_file} not found.")
+
+    # load config file(s)
+    sim_params = load_params(config_file)
+
+    # create BMCTool object and run simulation
+    sim = FID(sim_params, seq_file, adc_time, **kwargs)
+    sim.run_fid()
+    
 
     if show_plot:
         if "offsets" in kwargs:
@@ -69,6 +121,7 @@ def simulate(config_file: Union[str, Path], seq_file: Union[str, Path], show_plo
 #     simulate(
 #         config_file=config_file, seq_file=seq_file, show_plot=True, title="WASABI example spectrum", normalize=True
 #     )
+
 
 
 # if __name__ == "__main__":
